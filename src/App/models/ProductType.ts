85dd6c528693fac36e2dc79a6f1b92f7b6770711
dev-michel324 <dev_michel324@protonmail.com>
2022-12-14 20:41:33 -0300

    feat: added new entity

---
+++ src/App/models/ProductType.ts
@@ -0,0 +1,27 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    OneToMany
+} from "typeorm";
+import { Product } from "./Product";
+
+@Entity()
+export class ProductType {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({type: 'varchar', length: 45})
+    tipo: string
+
+    @OneToMany(() => Product, (product) => product.section)
+    products: Product[]
+
+    @CreateDateColumn({name: 'created_at'})
+    created_at: Date
+
+    @UpdateDateColumn({name: 'updated_at'})
+    updated_at: Date
+}
