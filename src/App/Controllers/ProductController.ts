48e49d34bbffa833673f6faf56a727614da5c021
dev-michel324 <dev_michel324@protonmail.com>
2022-12-14 20:39:02 -0300

    feat: added function to create a product

--- src/App/Controllers/ProductController.ts
+++ src/App/Controllers/ProductController.ts
@@ -1,7 +1,56 @@
+const dataSource = require('../../Database/dataSource');
+import {Product} from "../models/Product";
+import { productSchema, ProductType } from "../Validators/Product";
+
 class ProductController {
     
-    async index(req:any, res:any): Promise<string> {
-        return res.send('Products route');
+    async index(req:any, res:any): Promise<void> {
+        const users = await dataSource
+            .getRepository(Product)
+            .createQueryBuilder("product")
+            .getMany();
+
+        return res.json({
+                error: false,
+                users: users
+            });
+    }
+
+    async create(req:any, res:any): Promise<void> {
+        const product: ProductType = {
+            name: req.body.name,
+            available: req.body.available,
+            price: req.body.price
+        };
+
+        const validation = await productSchema.validate(product)
+            .catch(err => { return err; });
+        
+            if(validation.errors)
+                return res.status(400).json({
+                    error: true,
+                    product: product,
+                    message: validation.errors
+                });
+
+            try {
+                await dataSource
+                    .createQueryBuilder()
+                    .insert()
+                    .into(Product)
+                    .values(product)
+                    .execute();
+            } catch (err: any) {
+                return res.status(400).json({
+                    error: true,
+                    message: err
+                })
+            }
+        
+        return res.status(201).json({
+            error: false,
+            product: product
+        });
     }
 
 }
