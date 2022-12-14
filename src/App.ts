558a63a6893d169ff9fc311914b325e6e51357fa
dev-michel324 <dev_michel324@protonmail.com>
2022-12-14 20:37:46 -0300

    feat: added new route

--- src/App.ts
+++ src/App.ts
@@ -6,6 +6,7 @@ const productsController = require('./App/Controllers/ProductController');
 const port: string | number = process.env.SERVER_PORT || 3333;
 
 app.get('/products', productsController.index);
+app.post('/products', productsController.create);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
