4d0c0df784b78eb060123f3081ed2de467395869
dev-michel324 <dev_michel324@protonmail.com>
2022-12-15 09:36:53 -0300

    feat: added new schemas to validations

--- src/App/Validators/Product.ts
+++ src/App/Validators/Product.ts
@@ -6,17 +6,28 @@ import {
     InferType
 } from 'yup';
 
-const productCreateSchema = object({
-    name: string().required().min(1).max(55),
-    price: number().min(0.01).required(),
-    available: boolean().nullable(),
-});
-
-const productUpdateSchema = object({
+const productBaseSchema = object({
     id: number().required()
 });
 
-type ProductType = InferType<typeof productCreateSchema>;
+const productCreateSchema = object({
+    name: string().required().min(1).max(55),
+    price: number().min(0.01).required().positive(),
+    available: boolean()
+});
+
+const productUpdateSchema = object({})
+    .concat(productBaseSchema).concat(productCreateSchema);
+
+type ProductCreateType = InferType<typeof productCreateSchema>;
+type ProductBaseType = InferType<typeof productBaseSchema>;
 type ProductUpdateType = InferType<typeof productUpdateSchema>;
 
-export {productCreateSchema, productUpdateSchema, ProductType, ProductUpdateType};
+export {
+    productCreateSchema,
+    productBaseSchema,
+    ProductCreateType,
+    productUpdateSchema,
+    ProductBaseType,
+    ProductUpdateType
+};
