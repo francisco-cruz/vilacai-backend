d73f16f916a5ebd49a28f15b20724909550d0f88
dev-michel324 <dev_michel324@protonmail.com>
2022-12-14 22:01:43 -0300

    feat: added function to get product by id

--- src/App/Controllers/ProductController.ts
+++ src/App/Controllers/ProductController.ts
@@ -1,18 +1,24 @@
 const dataSource = require('../../Database/dataSource');
 import {Product} from "../models/Product";
-import { productSchema, ProductType } from "../Validators/Product";
+import {
+    productCreateSchema,
+    productUpdateSchema,
+    ProductType,
+    ProductUpdateType
+} from "../Validators/Product";
+import {ErrorHandler} from '../Errors/ErrorHandler';
 
 class ProductController {
     
     async index(req:any, res:any): Promise<void> {
-        const users = await dataSource
+        const products = await dataSource
             .getRepository(Product)
             .createQueryBuilder("product")
             .getMany();
 
         return res.json({
                 error: false,
-                users: users
+                products: products
             });
     }
 
@@ -23,33 +29,56 @@ class ProductController {
             price: req.body.price
         };
 
-        const validation = await productSchema.validate(product)
+        const validation = await productCreateSchema.validate(product)
             .catch(err => { return err; });
         
-            if(validation.errors)
-                return res.status(400).json({
-                    error: true,
-                    product: product,
-                    message: validation.errors
-                });
+        if(validation.errors)
+            return res.status(400).json(
+                new ErrorHandler(product, validation.errors).handle());
 
-            try {
-                await dataSource
-                    .createQueryBuilder()
-                    .insert()
-                    .into(Product)
-                    .values(product)
-                    .execute();
-            } catch (err: any) {
-                return res.status(400).json({
-                    error: true,
-                    message: err
-                })
-            }
+        try {
+            await dataSource
+                .createQueryBuilder()
+                .insert()
+                .into(Product)
+                .values(product)
+                .execute();
+        } catch (err: any) {
+            return res.status(400).json(
+                new ErrorHandler(product, err).handle());
+        }
         
         return res.status(201).json({
             error: false,
             product: product
+        });
+    }
+
+    async show(req:any, res:any): Promise<void> {
+        const product: ProductUpdateType = {
+            id: req.body.id
+        };
+
+        const validation = await productUpdateSchema.validate(product)
+            .catch(err => { return err; });
+        
+        if(validation.errors)
+            return res.status(400).json(
+                new ErrorHandler(product, validation.errors).handle());
+
+        const productFromDb = await dataSource
+                .getRepository(Product)
+                .createQueryBuilder("product")
+                .where("product.id = :id", { id: product.id })
+                .getOne();
+        
+        if(!productFromDb)
+            return res.status(404).json(
+                new ErrorHandler(product, "Product not found."));
+
+        return res.json({
+            error: false,
+            product: productFromDb
         });
     }
 
