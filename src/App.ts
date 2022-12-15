ef18e1f8fd1764c46a5a43220eb98fcb1e0ac4ec
dev-michel324 <dev_michel324@protonmail.com>
2022-12-14 22:01:20 -0300

    feat: added route to show product by id

--- src/App.ts
+++ src/App.ts
@@ -6,6 +6,7 @@ const productsController = require('./App/Controllers/ProductController');
 const port: string | number = process.env.SERVER_PORT || 3333;
 
 app.get('/products', productsController.index);
+app.get('/products/show', productsController.show);
 app.post('/products', productsController.create);
 
 app.listen(port, () => {
