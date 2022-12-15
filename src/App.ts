0aae6c34d22aee39fb3ea322643100dc7c246f36
dev-michel324 <dev_michel324@protonmail.com>
2022-12-15 11:16:25 -0300

    feat: added routes to section crud

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./Server');
 
 const productsController = require('./App/Controllers/ProductController');
+const sectionController = require('./App/Controllers/SectionController');
 
 const port: string | number = process.env.SERVER_PORT || 3333;
 
@@ -10,6 +11,12 @@ app.get('/products/show', productsController.show);
 app.post('/products', productsController.create);
 app.delete('/products', productsController.remove);
 app.put('/products', productsController.update);
+
+app.get('/sections', sectionController.index);
+app.post('/sections', sectionController.create);
+app.get('/sections/show', sectionController.show);
+app.delete('/sections', sectionController.remove);
+app.put('/sections', sectionController.update);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
