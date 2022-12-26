6596acab3623a5b55a7748e209dd62f69290904e
dev-michel324 <dev_michel324@protonmail.com>
2022-12-26 07:59:23 -0300

    feat: added type of product validation

--- src/App/Validators/Product.ts
+++ src/App/Validators/Product.ts
@@ -14,12 +14,18 @@ const productCreateSchema = object({
     section: number().required().integer().min(1).positive(),
     name: string().required().min(1).max(55),
     price: number().min(0.01).required().positive(),
-    available: boolean()
+    available: boolean(),
+    qntdMaxFilling: number().integer().min(1).positive()
 });
 
 const productAddFillingSchema = object({
     productId: number().required().integer().positive(),
     fillingId: number().required().integer().positive()
+});
+
+const productTypeSchema = object({
+    productId: number().required().integer().positive(),
+    typeId: number().required().integer().positive()
 });
 
 const productUpdateSchema = object({})
@@ -29,6 +35,7 @@ type ProductCreateType = InferType<typeof productCreateSchema>;
 type ProductBaseType = InferType<typeof productBaseSchema>;
 type ProductUpdateType = InferType<typeof productUpdateSchema>;
 type ProductAddFillingType = InferType<typeof productAddFillingSchema>;
+type ProductTypeType = InferType<typeof productTypeSchema>;
 
 export {
     productCreateSchema,
@@ -38,5 +45,7 @@ export {
     ProductBaseType,
     ProductUpdateType,
     ProductAddFillingType,
-    productAddFillingSchema
+    productAddFillingSchema,
+    productTypeSchema, 
+    ProductTypeType
 };
